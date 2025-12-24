42b7904216047556ac6ea23d20054ec70a9660c6
Mehrob02 <mehrobjonabdulvahobov@gmail.com>
2025-12-24 16:08:24 +0500

    new feature: packages

---
+++ lib/src/features/package/models/package.dart
@@ -0,0 +1,11 @@
+class Package {
+  final int id;
+  final String name;
+  final List<int> documentsIds;
+
+  Package({
+    required this.id,
+    required this.name,
+    required this.documentsIds,
+  });
+}
